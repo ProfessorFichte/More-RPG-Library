10122d851c2482ebec6e40781fe5bb37fb095f76
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-12-08 23:33:58 +0100

    Add Passive Spells used by T5 Weapons

---
+++ common/src/main/java/net/more_rpg_classes/custom/MrpgLibSpells.java
@@ -0,0 +1,145 @@
+package net.more_rpg_classes.custom;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.util.Identifier;
+import net.more_rpg_classes.effect.MRPGCEffects;
+import net.more_rpg_classes.sounds.ModSounds;
+import net.spell_engine.api.datagen.SpellBuilder;
+import net.spell_engine.api.spell.ExternalSpellSchools;
+import net.spell_engine.api.spell.Spell;
+import net.spell_engine.api.spell.fx.ParticleBatch;
+import net.spell_engine.api.spell.fx.Sound;
+import net.spell_engine.api.util.TriState;
+import net.spell_engine.client.gui.SpellTooltip;
+import net.spell_engine.client.util.Color;
+import net.spell_engine.fx.SpellEngineParticles;
+import net.spell_engine.internals.target.SpellTarget;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import static net.more_rpg_classes.MRPGCMod.MOD_ID;
+
+public class MrpgLibSpells {
+    public enum Category {
+        MELEE, RANGED, SPELL, HEAL, SHIELD
+    }
+    public record Entry(Identifier id, Spell spell, String title, String description,
+                        @Nullable SpellTooltip.DescriptionMutator mutator, EnumSet<Category> categories) {
+        public Entry(Identifier id, Spell spell, String title, String description,
+                     @Nullable SpellTooltip.DescriptionMutator mutator, Category category) {
+            this(id, spell, title, description, mutator, EnumSet.of(category));
+        }
+    }
+
+
+    public static final List<Entry> entries = new ArrayList<>();
+
+    private static Entry add(Entry entry) {
+        entries.add(entry);
+        return entry;
+    }
+
+    private static Spell.Impact.TargetModifier createImpactModifier(String entityType) {
+        var condition = new Spell.TargetCondition();
+        condition.entity_type = entityType;
+        var modifier = new Spell.Impact.TargetModifier();
+        modifier.conditions = List.of(condition);
+        return modifier;
+    }
+    private static void bleedingDeny(Spell.Impact impact) {
+        var modifier = createImpactModifier("#more_rpg_classes:bleeding_immune");
+        modifier.execute = TriState.DENY;
+        impact.target_modifiers = List.of(modifier);
+    }
+    private static Spell passiveSpellBase() {
+        var spell = new Spell();
+        spell.range = 0;
+        spell.tier = 8;
+
+        spell.type = Spell.Type.PASSIVE;
+        spell.passive = new Spell.Passive();
+        return spell;
+    }
+    ///PASSIVES
+    public static Entry carve_melee = add(carve_melee());
+    private static Entry carve_melee() {
+        var id = Identifier.of(MOD_ID, "carve");
+        var title = "Carve";
+        var description = "On melee hit: {trigger_chance} chance to stack armor reduction by {bonus2} and increasing incoming damage by {bonus} for {effect_amplifier_cap} times.";
+        var spell = passiveSpellBase();
+        spell.school = ExternalSpellSchools.PHYSICAL_MELEE;
+        var debuffEffect = MRPGCEffects.CARVE;
+        SpellTooltip.DescriptionMutator mutator = (args) -> {
+            var modifier = debuffEffect.config().attributes().get(1);
+            var modifier2 = debuffEffect.config().attributes().get(0);
+            var bonus = SpellTooltip.bonus(modifier.value, modifier.operation);
+            var bonus2 = SpellTooltip.bonus(modifier2.value, modifier2.operation);
+            return args.description()
+                    .replace("{bonus}", bonus)
+                    .replace("{bonus2}", bonus2);
+        };
+
+        var trigger = new Spell.Trigger();
+        trigger.type = Spell.Trigger.Type.MELEE_IMPACT;
+        trigger.equipment_condition = EquipmentSlot.MAINHAND;
+        trigger.chance = 0.2F;
+        spell.passive.triggers = List.of(trigger);
+
+        spell.target.type = Spell.Target.Type.FROM_TRIGGER;
+
+        var debuff = SpellBuilder.Impacts.effectAdd(debuffEffect.id.toString(), 7,1,5);
+        debuff.action.status_effect.apply_mode = Spell.Impact.Action.StatusEffect.ApplyMode.ADD;
+        debuff.action.status_effect.show_particles = false;
+        debuff.action.status_effect.amplifier = 1;
+        debuff.action.status_effect.amplifier_cap = 5;
+        debuff.action.status_effect.duration = 8;
+        debuff.particles = new ParticleBatch[]{
+                new ParticleBatch(
+                        SpellEngineParticles.MagicParticles.get(
+                                SpellEngineParticles.MagicParticles.Shape.SKULL,
+                                SpellEngineParticles.MagicParticles.Motion.DECELERATE).id().toString(),
+                        ParticleBatch.Shape.SPHERE, ParticleBatch.Origin.CENTER,
+                        25, 0.2F, 0.25F)
+                        .color(Color.RAGE.toRGBA())
+        };
+        debuff.sound = new Sound(ModSounds.CARVE_ID.toString());
+        spell.impacts = List.of(debuff);
+
+        SpellBuilder.Cost.cooldown(spell, 3);
+        spell.cost.batching = true;
+
+        return new Entry(id, spell, title, description, mutator,Category.MELEE);
+    }
+    public static Entry lightning_strike_melee = add(lightning_strike_melee());
+    private static Entry lightning_strike_melee() {
+        var id = Identifier.of(MOD_ID, "lightning_strike_melee");
+        var title = "Lightning Strike";
+        var description = "On melee hit: {trigger_chance} chance to spawn a lightning strike.";
+        var spell = passiveSpellBase();
+        spell.school = ExternalSpellSchools.PHYSICAL_MELEE;
+
+        var trigger = new Spell.Trigger();
+        trigger.type = Spell.Trigger.Type.MELEE_IMPACT;
+        trigger.equipment_condition = EquipmentSlot.MAINHAND;
+        trigger.chance = 0.15F;
+        spell.passive.triggers = List.of(trigger);
+
+        spell.target.type = Spell.Target.Type.FROM_TRIGGER;
+
+        var custom = new Spell.Impact();
+        custom.action = new Spell.Impact.Action();
+        custom.action.custom = new Spell.Impact.Action.Custom();
+        custom.action.type = Spell.Impact.Action.Type.CUSTOM;
+        custom.action.custom.intent = SpellTarget.Intent.HARMFUL;
+        custom.action.custom.handler = "more_rpg_classes:lightning";
+        spell.impacts = List.of(custom);
+
+        SpellBuilder.Cost.cooldown(spell, 5);
+        spell.cost.batching = true;
+
+        return new Entry(id, spell, title, description, null,Category.MELEE);
+    }
+}
